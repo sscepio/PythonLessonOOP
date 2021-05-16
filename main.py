276230a5e9c4f034cf6921d89693ab62c76b04f9
ПК <sscepio@yandex.ru>
2021-05-16 22:29:46 +0300

    сделал третье задание

--- main.py
+++ main.py
@@ -45,6 +45,50 @@ class Road:
 road = Road(20, 5000)
 road.calculate()
 
+# 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (
+# должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker.
+# В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (
+# get_total_income). Проверить работу примера на реальных данных (создать экземпляры класса Position,
+# передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+income = {}
+class Worker:
+    name = ""
+    surname = ""
+    position = ""
+    wage = 0
+    bonus = 0
+    _income = income
+    def __init__(self, name, surname, position, wage, bonus):
+         self._name = name
+         self._surname = surname
+         self._position = position
+         self._wage = wage
+         self._bonus = bonus
+         self._income[f"bonus{name}{surname}"] = bonus
+         self._income[f"wage{name}{surname}"] = wage
+    def workerInfo(self):
+        print(f'Имя {self._name} Фамилия {self._surname} Должность {self._position}', self._income[f"wage{self._name}{self._surname}"], self._income[f"bonus{self._name}{self._surname}"])
+worker = Worker ("Гарегин", "Папян", "Директор клиента", 120000, 20000)
+print(worker.workerInfo())
+print(income)
+class Position(Worker):
+    def getFullName(self):
+        print(self._name)
+
+
+position = Position("Гарегин", "Папян", "Директор клиента", 150000, 10000)
+position.getFullName()
+position.workerInfo()
+
+# 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (
+# булево). А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась,
+# повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый
+# класс метод show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar
+# переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться
+# сообщение о превышении скорости. Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к
+# атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
+
 
 
 
